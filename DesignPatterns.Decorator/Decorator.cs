abc89e633d435a1584254915d44f7ccfa4e07912
jasiu648 <jasiu648@gmail.com>
2024-10-01 11:46:59 +0200

    added decorator logic

--- DesignPatterns.Decorator/Decorator.cs
+++ DesignPatterns.Decorator/Decorator.cs
@@ -1,12 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DesignPatterns.Decorator
 {
-    internal class Decorator
+    public class DataFrame
     {
+        public string Data;
     }
+    public interface IDataSource
+    {
+        public void WriteData(DataFrame dataFrame);
+        public DataFrame ReadData();
+    }
+
+    public class FileDataSource : IDataSource
+    {
+        private string _filePath;
+
+        public FileDataSource(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public void WriteData(DataFrame dataFrame)
+        {
+            File.WriteAllText(_filePath, dataFrame.Data);
+        }
+        public DataFrame ReadData()
+        {
+            var content = File.ReadAllText(_filePath);
+            var dataFrame = new DataFrame();
+            dataFrame.Data = content;
+            return dataFrame;
+        }
+    }
+
+    public class DataSourceDecotator : IDataSource
+    {
+        protected IDataSource _dataSource;
+
+        public DataSourceDecotator(IDataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+        public virtual void WriteData(DataFrame dataFrame)
+        {
+            _dataSource.WriteData(dataFrame);
+        }
+        public virtual DataFrame ReadData()
+        {
+            return _dataSource.ReadData();
+        }
+    }
+
+    public class EncryptionDataSourceDecorator : DataSourceDecotator
+    {
+        public EncryptionDataSourceDecorator(IDataSource dataSource) : base(dataSource)
+        {
+        }
+        
+        public override void WriteData(DataFrame dataFrame)
+        {
+            var encryptedString = EncryptString(dataFrame.Data);
+            dataFrame.Data = encryptedString;
+            _dataSource.WriteData(dataFrame);
+        }
+        public override DataFrame ReadData()
+        {
+            var dataFrame = base.ReadData();
+            var decryptedString = DecryptString(dataFrame.Data);
+            dataFrame.Data = decryptedString;
+            return dataFrame;
+        }
+
+        private static string EncryptString(string plainText)
+        {
+            byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+        private static string DecryptString(string obfuscatedText)
+        {
+            byte[] obfuscatedBytes = Convert.FromBase64String(obfuscatedText);
+            return System.Text.Encoding.UTF8.GetString(obfuscatedBytes);
+        }
+    }
+
 }
--- DesignPatterns.Decorator/Program.cs
+++ DesignPatterns.Decorator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.Decorator;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+string projectRoot = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
+string filePath = Path.Combine(projectRoot, "testFile.txt");
+
+var dataSource = new FileDataSource(filePath);
+var encryptedDataSource = new EncryptionDataSourceDecorator(dataSource);
+
+var dataFrame = new DataFrame();
+dataFrame.Data = "This is test message";
+
+encryptedDataSource.WriteData(dataFrame);
+
+
+var dataFrameFromFile = encryptedDataSource.ReadData();
+Console.WriteLine(dataFrameFromFile.Data);
